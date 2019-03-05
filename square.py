74c8977e6da3efdaf04997d88394988497197abb
mritunjaysharma394 <45476902+mritunjaysharma394@users.noreply.github.com>
2019-03-05 23:18:23 +0530

    Add files via upload

---
+++ square.py
@@ -0,0 +1,29 @@
+import turtle
+
+def draw_square():
+    window= turtle.Screen()
+    window.bgcolor("red")
+    i=0
+    brad= turtle.Turtle()
+    brad.shape("turtle")
+    brad.color("yellow")
+    brad.speed(2)
+    for r in range(36):
+        
+        
+        for i in range(4):
+            
+            
+        
+        
+            brad.forward(100)
+            brad.right(90)
+            i=i+1
+        brad.rt(10)
+        r=r+1
+            
+    
+
+    
+draw_square()
+
